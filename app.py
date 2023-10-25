4fd2d9e48f9e0a9d1fb70be45844953357808fc2
HAMZA BAHJAOUI <94017550+hamzabx01@users.noreply.github.com>
2023-10-25 13:28:18 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,33 +1,38 @@
 from fastapi import FastAPI
-from fastapi.responses import RedirectResponse
-from textblob import TextBlob
-#import uvicorn
+from fastapi.responses import FileResponse
+from bs4 import BeautifulSoup
+import requests
+from datetime import datetime
+import json, os
 
-app = FastAPI(title="Sentiment Analysis API")
+app = FastAPI()
 
-@app.get("/", include_in_schema=False)
-def index():
-    return RedirectResponse("/docs", status_code=308)
+@app.get("/")
+def read_root():
+  return {"Hello": "World"}
 
-@app.get("/sentiment-analysis/{text}")
-def sentiment_analysis(text: str):
-    blob = TextBlob(text)
-    polarity = blob.sentiment.polarity
-    subjectivity = blob.sentiment.subjectivity
-
-    if polarity > 0:
-        sentiment = "positive"
-    elif polarity < 0:
-        sentiment = "negative"
-    else:
-        sentiment = "neutral"
-
-    return {
-        "text": text,
-        "sentiment": sentiment,
-        "polarity": polarity,
-        "subjectivity": subjectivity
-    }
-
-#if __name__ == '__main__':
-    #uvicorn.run('app:app', host='0.0.0.0', port=8000)
+@app.get("/data/{id}")
+def read_data(id: str):
+  url = r'https://www.emploi-public.ma/ar/index.asp?p='
+  start = 1
+  data = []
+  break_loop = False
+  while True:
+    response = requests.get(url + str(start))
+    response.encoding = 'utf-8'
+    soup = BeautifulSoup(response.text, 'html.parser')
+    table = soup.select_one('body > main > div > div > div.card.mt-3.mb-3 > div > div > table > tbody')
+    tr = table.find_all('tr')
+    for i in tr:
+      atag = i.select_one('td:nth-child(3) > a')
+      datetag = i.select_one('td:nth-child(2)').text
+      dateTody = datetime.now().strftime("%d/%m/%Y")
+      datediff = datetime.strptime(dateTody, "%d/%m/%Y") - datetime.strptime(datetag, "%d/%m/%Y")
+      if datediff.days == int(id):
+        break_loop = True
+        break
+      data.append([datetag, atag.text, "https://www.emploi-public.ma/ar/" + atag['href']])
+    if break_loop: break
+    start += 1
+    data.remove(data[-1])
+  return data
